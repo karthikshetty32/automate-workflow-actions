ea81c3bad3e41156d388c55a9cca5c7ac56f3aa4
karthikshetty32 <58006183+karthikshetty32@users.noreply.github.com>
2022-01-03 20:54:52 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+meimport logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Automate the workflow using GitHub Actions
         </p>
         <a
           className="App-link"
