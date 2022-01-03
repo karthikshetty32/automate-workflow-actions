6e72425b3898701d8c9e03ff8a65885825fe00c6
karthikshetty32 <58006183+karthikshetty32@users.noreply.github.com>
2022-01-03 21:21:27 +0530

    updated file

--- src/App.js
+++ src/App.js
@@ -7,20 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Automate the workflow using GitHub Actions
+          Hello KARTHIK SHETTY AND MANJUNATHA 
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        > 
+        >
           Learn React
-        </a>
-
-       
-      </header>
-    </div>
+       </a>
+    </header>
+  </div>
   );
 }
 
