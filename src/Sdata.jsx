cb893df7bc809914e304e345ac662917e2b580a8
Manjunatha V Ambore <90604826+mvambore@users.noreply.github.com>
2022-01-31 16:19:50 +0530

    course changed

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,9 +1,9 @@
 import course1 from "../src/images/course1.jpeg";
 import course2 from "../src/images/course2.jpeg";
 import course3 from "../src/images/course3.jpeg";
-import course4 from "../src/images/course4.jpeg";
-import course5 from "../src/images/course5.jpeg";
-import course6 from "../src/images/course6.jpeg";
+//import course4 from "../src/images/course4.jpeg";
+//import course5 from "../src/images/course5.jpeg";
+//import course6 from "../src/images/course6.jpeg";
 
 const Sdata = [
   {
